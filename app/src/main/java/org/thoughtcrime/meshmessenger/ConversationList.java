b1dd284db878875f1ebe29369cb27cb40568a41d
Albert Armea <github@albertarmea.com>
2016-11-23 20:10:36 -0600

    Update title bar name

--- app/src/main/java/org/thoughtcrime/meshmessenger/ConversationList.java
+++ app/src/main/java/org/thoughtcrime/meshmessenger/ConversationList.java
@@ -18,6 +18,8 @@ public class ConversationList extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        setTitle(R.string.app_name);
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
