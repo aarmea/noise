fabb95bfca936db33deed4bb08d7da0ce708188c
Albert Armea <github@albertarmea.com>
2017-02-05 23:17:52 -0800

    Add a dummy conversation list view
    While I do eventually intend to populate this view, for now it just exists to
    help finalize the view structure so that the debug views have a more permanent
    home in its options menu.

---
+++ app/src/main/java/com/alternativeinfrastructures/noise/views/ConversationList.java
@@ -0,0 +1,56 @@
+package com.alternativeinfrastructures.noise.views;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.alternativeinfrastructures.noise.R;
+
+public class ConversationList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conversation_list);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        setTitle(R.string.conversation_view_title);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    // TODO: Implement conversations here
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_conversation_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        // TODO: Hide the debug-oriented options in release builds
+        if (id == R.id.action_raw_message_list) {
+            startActivity(new Intent(this, RawMessageList.class));
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
