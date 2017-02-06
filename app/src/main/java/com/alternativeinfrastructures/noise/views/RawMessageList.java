5ac206fcef0655769aaf7f74a28cdad96981d2ad
Albert Armea <github@albertarmea.com>
2017-02-05 23:28:14 -0800

    Cleanup: delete the menu from the raw message list

--- app/src/main/java/com/alternativeinfrastructures/noise/views/RawMessageList.java
+++ app/src/main/java/com/alternativeinfrastructures/noise/views/RawMessageList.java
@@ -6,8 +6,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -85,27 +83,5 @@ public class RawMessageList extends AppCompatActivity {
                 makeDummyMessageTransaction.execute();
             }
         });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_raw_message_list, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
