d0075b02e599e8a825269940d70ef8ba8572bf02
Albert Armea <albert@albertarmea.com>
2017-03-10 07:24:31 -0800

    Cleanup: Remove unneecessary imports

--- app/src/main/java/com/alternativeinfrastructures/noise/NoiseApplication.java
+++ app/src/main/java/com/alternativeinfrastructures/noise/NoiseApplication.java
@@ -1,7 +1,6 @@
 package com.alternativeinfrastructures.noise;
 
 import android.app.Application;
-import android.content.Intent;
 
 import com.raizlabs.android.dbflow.config.FlowConfig;
 import com.raizlabs.android.dbflow.config.FlowManager;
