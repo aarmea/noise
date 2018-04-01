e550d5f53fbe358954bfcb61155f8ab23684f2bb
Albert Armea <albert@albertarmea.com>
2018-03-31 23:19:05 -0700

    Kick the BluetoothSyncService when the MAC address setting changes
    Fixes #4

--- app/src/main/java/com/alternativeinfrastructures/noise/views/SettingsActivity.java
+++ app/src/main/java/com/alternativeinfrastructures/noise/views/SettingsActivity.java
@@ -2,14 +2,20 @@ package com.alternativeinfrastructures.noise.views;
 
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 
 import com.alternativeinfrastructures.noise.R;
+import com.alternativeinfrastructures.noise.sync.bluetooth.BluetoothSyncService;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity
+        implements SharedPreferences.OnSharedPreferenceChangeListener {
+    public static final String TAG = "SettingsActivity";
     public static final String KEY_BLUETOOTH_MAC = "pref_key_bluetooth_mac";
 
     @Override
@@ -23,10 +29,36 @@ public class SettingsActivity extends AppCompatActivity {
 
         FragmentManager fragmentManager = getFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        NoisePrefsFragment prefsFragment = new NoisePrefsFragment();
+        prefsFragment = new NoisePrefsFragment();
         fragmentTransaction.replace(R.id.content_settings_placeholder, prefsFragment);
         fragmentTransaction.commit();
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        prefsFragment.getPreferenceScreen().getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        prefsFragment.getPreferenceScreen().getSharedPreferences()
+                .unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences preferences, String key) {
+        if (key.equals(KEY_BLUETOOTH_MAC)) {
+            Log.d(TAG, "Bluetooth MAC changed, kicking service");
+            Intent stopSyncServiceIntent = new Intent(this, BluetoothSyncService.class);
+            stopService(stopSyncServiceIntent);
+            BluetoothSyncService.startOrPromptBluetooth(this);
+        }
+    }
+
+    NoisePrefsFragment prefsFragment;
 
     public static class NoisePrefsFragment extends PreferenceFragment {
         @Override
